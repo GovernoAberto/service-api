cfb3765cdba921634b7f9f9d4534b83fef489c10
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2021-04-25 04:01:14 -0300

    Ajustando eventos das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -14,11 +14,11 @@ router.get('/state/:alias/cities', cityController.cities);
 router.get('/city-info/:state/:city', cityController.cityInfo);
 router.get('/dataset/add', datasetController.add);
 router.get('/dataset/:name', routeEvents, datasetController.dataset);
-router.get('/dataset/:name/table', routeEvents, datasetController.datasetTable);
+router.get('/dataset/:name/table', datasetController.datasetTable);
 router.get('/dataset/:name/download/:format', routeEvents, datasetController.downloadDataset);
 router.get('/categories', datasetController.categories);
 router.get('/visualization/:alias', routeEvents, datasetController.visualization);
-router.get('/visualization/:alias/table', routeEvents, datasetController.tableByVisualization);
+router.get('/visualization/:alias/table', datasetController.tableByVisualization);
 router.get('/visualization/:alias/download/:format', routeEvents, datasetController.downloadVisualizationTable);
 router.get('/visualizations', routeEvents, datasetController.visualizations);
 
